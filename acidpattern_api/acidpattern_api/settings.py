a8c6e487db6308fd7ece8f247db161d28430af86
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-04-15 14:02:22 -0700

    update .gitignore

--- acidpattern_api/acidpattern_api/settings.py
+++ acidpattern_api/acidpattern_api/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
 
-LOGIN_URL = 'auth/login'
+LOGIN_URL = '/admin/login/'
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': (
